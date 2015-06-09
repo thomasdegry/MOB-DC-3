d563ff290a275e01a8af5c57fb367b81ea69af05
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-06-08 21:15:40 -0400

    Added sample apps. Added Lesson 2 slides

---
+++ Classwork/Week 1/BioApp/BioApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BioApp
+//
+//  Created by Arthur Sabintsev on 6/8/15.
+//  Copyright (c) 2015 Arthur Ariel Sabintsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
