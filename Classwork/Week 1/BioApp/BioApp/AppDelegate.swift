8a9d60483d9533a4a63fb07d61fff8afd46bdd16
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-06-08 21:19:36 -0400

    Create Bio App
    Played with Segues

--- Classwork/Week 1/BioApp/BioApp/AppDelegate.swift
+++ Classwork/Week 1/BioApp/BioApp/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    let test: String?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
